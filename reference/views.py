0b79ba81cc587b7a9e338ed88fd433d282476b6a
Amrit Hariharan <amrit.harih@gmail.com>
2019-02-09 17:05:47 -0500

    working on freezing

--- reference/views.py
+++ reference/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, Markup, render_template
 from os import listdir, path, walk
-from reference import app
+from reference import app, freezer
 import re
 
 
@@ -16,11 +16,22 @@ def reference():
             for filename in sorted(listdir(path.join(commands_path, category))):
                 filestring = open(path.join(commands_path, category, filename)).read()
                 filecontents = re.split('\n--+ *\n', filestring, flags=re.MULTILINE)
-                commands[category].append((filename, filecontents[0], filecontents[1]))
+                commands[category].append((path.splitext(filename)[0], filecontents[0], filecontents[1]))
     return render_template('index.html', allFiles=commands)
 
 
 @app.route('/reference/<category>/<name>')
 def command(category, name):
-    content = open(path.join(app.static_folder, 'commands', category, name)).read()
+    content = open(path.join(app.static_folder, 'commands', category, name + '.md')).read()
     return render_template('command.html', name=name, documentation=content)
+
+
+@freezer.register_generator
+def command():
+    commands_path = path.join(app.static_folder, 'commands')
+    for _, subdirList, _ in walk(commands_path):
+        # over CATEGORIES
+        for category in subdirList:
+            # over COMMANDS in category
+            for filename in sorted(listdir(path.join(commands_path, category))):
+                yield {'category': category, 'name': path.splitext(filename)[0]}
